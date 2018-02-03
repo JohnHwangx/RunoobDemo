311ded29989c14bdd3d9ba883b8c62112b817178
huangqiang <johnhuangx@outlook.com>
2018-02-03 14:46:15 +0800

    3

--- app/src/main/java/com/johnh/runoobdemo/No4_AppComponents/No4_4_1_ContentProvider1.java
+++ app/src/main/java/com/johnh/runoobdemo/No4_AppComponents/No4_4_1_ContentProvider1.java
@@ -1,20 +1,33 @@
 package com.johnh.runoobdemo.No4_AppComponents;
 
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.OperationApplicationException;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.johnh.runoobdemo.R;
 
+import java.util.ArrayList;
+
 public class No4_4_1_ContentProvider1 extends AppCompatActivity {
+    private EditText editNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no4_4_1__content_provider1);
+
+        editNum=findViewById(R.id.editNum);
     }
 
     private void getMsgs() {
@@ -36,7 +49,105 @@ public class No4_4_1_ContentProvider1 extends AppCompatActivity {
         cursor.close();
     }
 
+    private void insertMsg() {
+        ContentResolver resolver = getContentResolver();
+        Uri uri = Uri.parse("content://sms/");
+        ContentValues conValues = new ContentValues();
+        conValues.put("address", "123456789");
+        conValues.put("type", 1);
+        conValues.put("date", System.currentTimeMillis());
+        conValues.put("body", "no zuo no die why you try!");
+        resolver.insert(uri, conValues);
+        Log.e("HeHe", "短信插入完毕~");
+    }
+
+    private void getContacts(){
+        //①查询raw_contacts表获得联系人的id
+        ContentResolver resolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        //查询联系人数据
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        while(cursor.moveToNext())
+        {
+            //获取联系人姓名,手机号码
+            String cName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String cNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            System.out.println("姓名:" + cName);
+            System.out.println("号码:" + cNum);
+            System.out.println("======================");
+        }
+        cursor.close();
+    }
+
+    private void queryContact(String number){
+        Uri uri = Uri.parse("content://com.android.contacts/data/phones/filter/" + number);
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(uri, new String[]{"display_name"}, null, null, null);
+        if (cursor.moveToFirst()) {
+            String name = cursor.getString(0);
+            System.out.println(number + "对应的联系人名称：" + name);
+        }
+        cursor.close();
+    }
+
+    private void AddContact() throws RemoteException, OperationApplicationException {
+        //使用事务添加联系人
+        Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
+        Uri dataUri =  Uri.parse("content://com.android.contacts/data");
+
+        ContentResolver resolver = getContentResolver();
+        ArrayList<ContentProviderOperation> operations = new ArrayList<>();
+        ContentProviderOperation op1 = ContentProviderOperation.newInsert(uri)
+                .withValue("account_name", null)
+                .build();
+        operations.add(op1);
+
+        //依次是姓名，号码，邮编
+        ContentProviderOperation op2 = ContentProviderOperation.newInsert(dataUri)
+                .withValueBackReference("raw_contact_id", 0)
+                .withValue("mimetype", "vnd.android.cursor.item/name")
+                .withValue("data2", "Coder-pig")
+                .build();
+        operations.add(op2);
+
+        ContentProviderOperation op3 = ContentProviderOperation.newInsert(dataUri)
+                .withValueBackReference("raw_contact_id", 0)
+                .withValue("mimetype", "vnd.android.cursor.item/phone_v2")
+                .withValue("data1", "13798988888")
+                .withValue("data2", "2")
+                .build();
+        operations.add(op3);
+
+        ContentProviderOperation op4 = ContentProviderOperation.newInsert(dataUri)
+                .withValueBackReference("raw_contact_id", 0)
+                .withValue("mimetype", "vnd.android.cursor.item/email_v2")
+                .withValue("data1", "779878443@qq.com")
+                .withValue("data2", "2")
+                .build();
+        operations.add(op4);
+        //将上述内容添加到手机联系人中~
+        resolver.applyBatch("com.android.contacts", operations);
+        Toast.makeText(getApplicationContext(), "添加成功", Toast.LENGTH_SHORT).show();
+    }
+
     public void SimpleReadSms(View view) {
         getMsgs();
+    }
+
+    public void SimpleInsertSms(View view) {
+        insertMsg();
+    }
+
+    public void SimpleReadContact(View view) {
+        getContacts();
+    }
+
+    public void SimpleQueryContact(View view) {
+        String number=editNum.getText().toString().trim();
+        queryContact(number);
+    }
+
+    public void SimpleAddContact(View view) throws RemoteException, OperationApplicationException {
+        AddContact();
     }
 }
